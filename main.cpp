4de476768c572ee89929bcab93cb97c6536944b5
Abdalla <aabdelgadir@myseneca.ca>
2025-05-14 16:45:10 -0400

    imgui configured & installed libraries

--- main.cpp
+++ main.cpp
@@ -1,16 +1,33 @@
+#include <GLFW/glfw3.h>
 #include <iostream>
 #include "Importer.h"
 
+
 int main() {
-    std::string path = "assets/passwords.csv"; 
-
-    // Import credentials
-    auto credentials = CSVImporter::importFromCSV(path);
-
-    // Display imported credentials
-    for (const auto& cred : credentials) { //range-based loop
-        cred->display();
+    if (!glfwInit()) {
+        return -1;
     }
+
+    GLFWwindow* window = glfwCreateWindow(1280, 720, "My App", NULL, NULL);
+    if (!window) {
+        glfwTerminate();
+        return -1;
+    }
+
+    glfwMakeContextCurrent(window);
+    glfwSwapInterval(1); // Enable V-Sync
+
+    while (!glfwWindowShouldClose(window)) {
+        glClear(GL_COLOR_BUFFER_BIT);
+
+        // Add rendering code here
+
+        glfwSwapBuffers(window);
+        glfwPollEvents();
+    }
+
+    glfwDestroyWindow(window);
+    glfwTerminate();
 
     return 0;
 }
